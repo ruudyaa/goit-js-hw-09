c349f96305cada24f48c267e4468c8e225eca0bf
ruudyaa <martarudnytska26@gmail.com>
2023-04-09 19:54:35 +0300

    Adds first task.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const CHANGE_COLOR_DELAY = 1000;
+let idInt = null;
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', onBtnStartColorChange);
+refs.btnStop.addEventListener('click', onBtnStopColorChange);
+
+function onBtnStartColorChange() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  idInt = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function onBtnStopColorChange() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+
+  clearInterval(idInt);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
